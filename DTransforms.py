76df8ef2ece7111f94a533249fe4eff5b5fc817a
Mher Baghinyan <mbaghinyan@gmail.com>
2016-04-06 15:19:57 +0400

    Added files via upload

---
+++ DTransforms.py
@@ -0,0 +1,23 @@
+from sympy import *
+
+t = Symbol('t')
+matrix = [[1, 2*t, 1/t],[-t, 2, 0],[t, t-1, t*(t -1)]]
+
+print(matrix)
+
+def differential(matrix, level):
+    "returns a differential of given matrix"
+    t = Symbol('t')
+    for i in range(0, len(matrix)):
+        for j in range(0, len(matrix)):
+            print(diff(matrix[i][j], t, level))
+    print(diff(sin(t), t))
+
+
+x = Symbol('x')
+y = Symbol('y')
+
+print(x + x + x)
+
+
+differential(matrix, 1)
