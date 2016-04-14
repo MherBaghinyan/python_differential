5e2effa468b1d413ec2f3eee5414a96ab5a715a9
mher-mbag <mher.baghinyan@plexonic.com>
2016-04-14 19:48:01 +0400

    gui grid example

---
+++ Matrix_GUI.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+rows = []
+for i in range(5):
+    cols = []
+    for j in range(4):
+        e = Entry(relief=RIDGE)
+        e.grid(row=i, column=j, sticky=NSEW)
+        e.insert(END, '%d.%d' % (i, j))
+        cols.append(e)
+    rows.append(cols)
+
+print(rows)
+mainloop()
