99aa126b49cdbec964821ec055f72f7762a9b7e2
mher-mbag <mher.baghinyan@plexonic.com>
2016-04-18 16:11:14 +0400

    corrections in P image calculations

--- Leverrier.py
+++ Leverrier.py
@@ -28,7 +28,6 @@ def multiply_image_matrix(matrix, k):
     z_matrix = [[0] * _length for x in range(_length)]
     for l in range(0, k + 1):
         z_matrix += np.dot(differential_transform(matrix, l), differential_transform(matrix, k - l))
-        print(z_matrix)
     return z_matrix
 
 def get_s_number(n_value, k_value):
@@ -40,22 +39,22 @@ def get_s_number(n_value, k_value):
     sum = 0
     for n in range(0, _length):
         sum += matrix[n][n]
-    print("s_ =", sum)
     return sum
 
 def get_p_image(n_value, k_value):
     item = 1 / n_value * get_s_number(n_value, k_value)
+    addition = 0
     for l in range(1, k_value + 1):
             for i in range(1, n_value - l + 1):
-                item = - (1 / n_value) * get_s_number(n_value - i, l) * get_p_image(i, k_value - l)
-    return item
+                addition +=  get_s_number(n_value - i, l) * get_p_image(i, k_value - l)
+    result = (1 / n_value) * (item - addition)
+    print("n = ", n_value, "k = ", k_value, "p = ", result)
+    return result
 
 def p_items( n, k_value):
     item = 0
     for k in range(1, k_value + 1):
-        print("p(k) = ", get_p_image(n, k))
         item += (t ** k / factorial(k)) * get_p_image(n, k)
-        print("n = ", item)
     return item
 
 def calculate_Leverrier(n_value, k_value):
@@ -63,8 +62,7 @@ def calculate_Leverrier(n_value, k_value):
     p_item = [0 for x in range(count)]
     for n in range(1, n_value + 1):
         p_item[n - 1] = p_items(n, k_value)
-        print(p_item[n - 1])
     return p_item
 
-k_max = get_max_k(S_matrix) + 1
+k_max = get_max_k(S_matrix)
 print(calculate_Leverrier(2, k_max))
