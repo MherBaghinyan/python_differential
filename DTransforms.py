3d25a73b1ca3c8291851a9fafd0960879d841d75
mher-mbag <mher.baghinyan@plexonic.com>
2016-04-19 17:58:17 +0400

    some k indices

--- DTransforms.py
+++ DTransforms.py
@@ -30,15 +30,15 @@ def x_image_transform(matrix, vector, index):
 def last_part(matrix, vector, k):
     _length = len(matrix)
     result = [[0] * _length for x in range(_length)]
-    for p in range(1, k):
+    for p in range(1, k + 1):
         if k > 0:
             result += (differential_transform(matrix, p) * x_image_transform(matrix, vector, k - p))
     return result
 
-def calculateTransform(start, end):
+def calculateTransform(k_max):
     x_item = [0 for x in range(len(C_vector))]
     a_inverse = transform_and_inverse(A_matrix, 0)
-    for k in range(start, end):
+    for k in range(0, k_max):
         last_p = last_part(A_matrix, C_vector, k)
         step = a_inverse * (differential_vector(C_vector, k) - last_p)
         x_item[k] = step * (t ** k)
@@ -46,7 +46,7 @@ def calculateTransform(start, end):
     return x_item
 
 k_max = get_max_k(A_matrix)
-print(calculateTransform(0, k_max))
+print(calculateTransform(k_max))
 
 
 # print(np.dot(A_matrix, A_matrix))
