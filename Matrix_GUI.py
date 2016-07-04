31e16ecb7cf3f36f4422372f1d6a4acf29914c8e
mher-mbag <mher.baghinyan@plexonic.com>
2016-07-04 12:52:39 +0400

    label and button added

--- Matrix_GUI.py
+++ Matrix_GUI.py
@@ -1,14 +1,26 @@
 from tkinter import *
 
-rows = []
-for i in range(5):
-    cols = []
-    for j in range(4):
-        e = Entry(relief=RIDGE)
-        e.grid(row=i, column=j, sticky=NSEW)
-        e.insert(END, '%d.%d' % (i, j))
-        cols.append(e)
-    rows.append(cols)
+root = Tk()
+root.geometry("500x500")
 
-print(rows)
-mainloop()
+w = Label(root, text="Insert the payoff matrix")
+w.pack()
+
+
+# rows = []
+# for i in range(5):
+#     cols = []
+#     for j in range(4):
+#         e = Entry(root, relief=RIDGE)
+#         e.grid(row=i, column=j, sticky=NSEW)
+#         e.insert(END, '%d.%d' % (i, j))
+#         cols.append(e)
+#     rows.append(cols)
+#
+# print(rows)
+
+
+button = Button(root, text='create the parametric linear programming  model', width=50, command=root.destroy)
+button.pack()
+
+root.mainloop()
