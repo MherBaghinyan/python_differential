e11f8df21a39501a4ac3446cfc4fe749ffab9ba6
mher-mbag <mher.baghinyan@plexonic.com>
2016-09-28 18:23:33 +0400

    simple dropdown with tkinter

--- PyThonGUI.py
+++ PyThonGUI.py
@@ -1,23 +1,41 @@
 __author__ = 'Gebruiker'
-
+#GUI link
+#http://www.python-course.eu/tkinter_layout_management.php
 from tkinter import *
 
 root = Tk()
-
-theLabel = Label(root, text="Game theory software")
-theLabel.pack()
+root.title("Parametric game model solver")
+root.geometry("500x500")
 
 
 def evaluate(event):
-    data = e.get()
-    answer.configure(text="Answer is= " + str(eval(data)))
+    data = transformationEntry.get()
+    transformationLabel.configure(text="K= " + str(eval(data)))
 
 
-e = Entry(root)
-e.bind('<Return>', evaluate)
-e.pack()
-answer = Label(root)
-answer.pack()
+matrixLabel = Label(root, text="Enter transformation level: ")
+matrixLabel.pack(padx=10, pady=1, side=LEFT)
+transformationEntry = Entry(root)
+transformationEntry.bind('<Return>', evaluate)
+transformationEntry.pack(padx=10, pady=1, side=LEFT)
+transformationLabel = Label(root)
+transformationLabel.pack(padx=10)
 
+
+var = StringVar(root)
+var.set("Cooperative")  # initial value
+
+option = OptionMenu(root, var, "Cooperative", "Multi parameter game", "One parameter game")
+option.pack(padx=10, pady=1, side=LEFT)
+
+# test stuff
+
+
+def ok():
+    print("value is", var.get())
+    root.quit()
+
+button = Button(root, text="OK", command=ok)
+button.pack(padx=10, pady=10)
 
 root.mainloop()
