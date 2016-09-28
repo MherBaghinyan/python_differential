c8546e2973d5a8ab884b741a07dbfc93de8209ed
mher-mbag <mher.baghinyan@plexonic.com>
2016-09-28 17:54:31 +0400

    simple UI get input value and display

--- PyThonGUI.py
+++ PyThonGUI.py
@@ -6,4 +6,18 @@ root = Tk()
 
 theLabel = Label(root, text="Game theory software")
 theLabel.pack()
+
+
+def evaluate(event):
+    data = e.get()
+    answer.configure(text="Answer is= " + str(eval(data)))
+
+
+e = Entry(root)
+e.bind('<Return>', evaluate)
+e.pack()
+answer = Label(root)
+answer.pack()
+
+
 root.mainloop()
