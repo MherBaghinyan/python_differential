6f3c33e528956cc85cba063a8d011c6ef2b6d89d
mherbaghinyan <mbaghinyan@gmail.com>
2016-12-03 22:40:24 +0400

    matrix gui changes

--- Matrix_GUI.py
+++ Matrix_GUI.py
@@ -3,19 +3,31 @@ from Multiparametric import *
 from tkinter import *
 from sympy.parsing.sympy_parser import parse_expr
 
+root = Tk()
+root.title("Multi Parametric game model solver")
+root.geometry("700x700")
+
+
 rows = []
 for i in range(5):
     cols = []
     for j in range(5):
-        e = Entry(relief=RIDGE)
+        e = Entry(root, relief=RIDGE)
         e.grid(row=i, column=j, sticky=NSEW)
         e.insert(END, 0.0)
         cols.append(e)
     rows.append(cols)
 
+v_recovered = ''
+z_matrix = [[0] * len(rows) for x in range(len(rows))]
+
+strategies_recovered = [0 for x in range(len(z_matrix))]
+
+v_label = Label(root, text=v_recovered).grid(row=0, column=6)
+
+s_label = Label(root, text=strategies_recovered).grid(row=1, column=7)
 
 def on_press():
-    z_matrix = [[0] * len(rows) for x in range(len(rows))]
     i = 0
     for row in rows:
         j = 0
@@ -26,14 +38,9 @@ def on_press():
         i += 1
         print()
 
-    v_recovered = 0
-    strategies_recovered = [0 for x in range(len(z_matrix))]
-    v_recovered = initiate_simplex_matrix(z_matrix, v_recovered, strategies_recovered)
+    recover_value = 0
+    v_recovered = initiate_simplex_matrix(z_matrix, recover_value, strategies_recovered)
 
-    v_label = Label(text=v_recovered)
-    v_label.pack()
-    s_label = Label(text=strategies_recovered)
-    s_label.pack()
 
-Button(text='Fetch', command=on_press).grid()
+Button(root, text='Solve', command=on_press).grid()
 mainloop()
