75d074a05c52ef351339fe326ecd20ffa11d67a0
mherbaghinyan <mbaghinyan@gmail.com>
2017-02-11 21:54:49 +0400

    working version of minimize

--- optimization_util.py
+++ optimization_util.py
@@ -2,18 +2,47 @@
 import math
 import numpy as np
 from scipy.optimize import minimize
+from transformation_util import *
 
 
 def func(x, sign=1.0):
         """ Objective function """
-        return sign*(x[0])
+        return sign * x[0]
 
 
-def nonlinear_optimality():
+def nonlinear_optimality(x_b_image_matrix, k, vector_len, t_value):
 
-    cons = ({'type': 'ineq', 'fun': lambda x: np.array([-(2*x[0] - 10)])},
+    parametric_array = [0 for x in range(vector_len)]
+
+    for i in range(0, k + 1):
+        for j in range(0, vector_len):
+            s_item = x_b_image_matrix[i][j] * ((t-t_value)**i)
+            parametric_array[j] += s_item
+
+    cons = []
+    for i in range(len(parametric_array)):
+        cons.append({'type': 'ineq', 'fun': lambda x: np.array([parametric_array[i].evalf(subs={t: x[0]})])})
+
+    # cons = [{'type': 'ineq', 'fun': lambda x: np.array([-(2*x - 10)])},
+    #         {'type': 'ineq', 'fun': lambda x: np.array([-(-x - 30)])},
+    #         {'type': 'ineq', 'fun': lambda x: np.array([-(7*x - 30)])}]
+
+    res = minimize(func, 0.0, args=(-1.0,), constraints=cons, method='SLSQP')
+
+    print(res.x)
+    print(math.isnan(float(res.x)))
+    return res.x
+
+
+def lamb(x):
+    return np.array([-(2*x - 10)])
+
+
+def test_optimize():
+
+    cons = [{'type': 'ineq', 'fun': lambda x: lamb(x[0])},
             {'type': 'ineq', 'fun': lambda x: np.array([-(-x[0] - 30)])},
-            {'type': 'ineq', 'fun': lambda x: np.array([-(7*x[0] - 30)])})
+            {'type': 'ineq', 'fun': lambda x: np.array([-(7*x[0] - 30)])}]
 
     res = minimize(func, [0.0], args=(-1.0,), constraints=cons, method='SLSQP')
 
@@ -21,4 +50,4 @@ def nonlinear_optimality():
     print(math.isnan(float(res.x)))
 
 
-nonlinear_optimality()
+# test_optimize()
