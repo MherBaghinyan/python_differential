1b89c50380b347d725d911d8e09e9dba37a0ff41
Mher Baghinyan <mbaghinyan@gmail.com>
2017-02-20 17:51:34 +0400

    Update optimization_util.py

--- optimization_util.py
+++ optimization_util.py
@@ -10,11 +10,6 @@ def func(x, sign=1.0):
         return sign * x[0]
 
 
-def func_z(z, sign=1.0):
-        """ Objective function """
-        return sign * z[0]
-
-
 def z_nonlinear_optimality(image_matrixes, x_b_image_matrix, k_value, vector_len, t_value):
 
     columns = len(image_matrixes[0][0])
@@ -24,16 +19,21 @@ def z_nonlinear_optimality(image_matrixes, x_b_image_matrix, k_value, vector_len
         image_matrix = image_matrixes[k]
         for i in range(1, columns):
             f_item = image_matrix[0][i] * ((t-t_value)**k)
-            f_array[i - 1] = f_item
+            f_array[i - 1] += f_item
 
     f_cons = []
+    solo_item = 0
     for i in range(len(f_array)):
         if not is_number(f_array[i]):
-            f_cons.append({'type': 'ineq', 'fun': lambda z: np.array([f_array[i].evalf(subs={t: z[0]})])})
+            solo_item = f_array[i]
+            f_cons.append({'type': 'ineq', 'fun': lambda x: np.array([f_array[i].evalf(subs={t: x[0]})])})
+
+    if len(f_cons) == 1:
+        f_cons = [{'type': 'ineq', 'fun': lambda x: np.array([solo_item.evalf(subs={t: x[0]})])}]
 
     if len(f_cons) > 0:
-        f_res = minimize(func_z, 0.0, args=(-1.0,), constraints=f_cons, method='SLSQP')
-        print(f_res.x[0])
+        f_res = minimize(func, 0.0, args=(-1.0,), constraints=f_cons, method='SLSQP')
+        print(f_res.x)
         return f_res.x[0]
 
     return math.nan
@@ -50,7 +50,8 @@ def x_b_nonlinear_optimality(image_matrixes, x_b_image_matrix, k_value, vector_l
 
     x_b_cons = []
     for i in range(len(x_b_array)):
-        x_b_cons.append({'type': 'ineq', 'fun': lambda x: np.array([x_b_array[i].evalf(subs={t: x[0]})])})
+        if not is_number(x_b_array[i]):
+            x_b_cons.append({'type': 'ineq', 'fun': lambda x: np.array([x_b_array[i].evalf(subs={t: x[0]})])})
 
     if len(x_b_cons) > 0:
         res = minimize(func, 0.0, args=(-1.0,), constraints=x_b_cons, method='SLSQP')
@@ -58,54 +59,6 @@ def x_b_nonlinear_optimality(image_matrixes, x_b_image_matrix, k_value, vector_l
         return res.x[0]
 
     return math.nan
-    #
-    # x_b_res = minimize(func, 0.0, args=(-1.0,), constraints=x_b_cons, method='SLSQP')
-    #
-    # print(x_b_res.x[0])
-    # if len(f_cons) > 0 and (t_value < x_b_res.x[0] < x_b_res.x[0]):
-    #     return f_res.x[0]
-    #
-    # return x_b_res.x[0]
-
-
-    # x_b_array = [0 for x in range(vector_len)]
-    # columns = len(image_matrixes[0][0])
-    # f_array = [0 for x in range(len(image_matrixes[0][0]) - 1)]
-    #
-    # for k in range(0, k_value + 1):
-    #     image_matrix = image_matrixes[k]
-    #     rows = len(image_matrix)
-    #     for j in range(1, rows):
-    #         s_item = image_matrix[j][0] * ((t-t_value)**k)
-    #         x_b_array[j - 1] += s_item
-    #     for i in range(1, columns):
-    #         f_item = image_matrix[0][i] * ((t-t_value)**k)
-    #         f_array[i - 1] += f_item
-    #
-    # x_b_cons = []
-    # for i in range(len(x_b_array)):
-    #     x_b_cons.append({'type': 'ineq', 'fun': lambda x: np.array([x_b_array[i].evalf(subs={t: x[0]})])})
-    #
-    # f_cons = []
-    # for i in range(len(f_array)):
-    #     if not is_number(f_array[i]):
-    #         f_cons.append({'type': 'ineq', 'fun': lambda x: np.array([f_array[i].evalf(subs={t: x[0]})])})
-    #
-    # # cons = [{'type': 'ineq', 'fun': lambda x: np.array([-(2*x - 10)])},
-    # #         {'type': 'ineq', 'fun': lambda x: np.array([-(-x - 30)])},
-    # #         {'type': 'ineq', 'fun': lambda x: np.array([-(7*x - 30)])}]
-    #
-    # if len(f_cons) > 0:
-    #     f_res = minimize(func, 0.0, args=(-1.0,), constraints=f_cons, method='SLSQP')
-    #
-    # x_b_res = minimize(func, 0.0, args=(-1.0,), constraints=x_b_cons, method='SLSQP')
-    #
-    # print(x_b_res.x[0])
-    # if len(f_cons) > 0 and (t_value < x_b_res.x[0] < x_b_res.x[0]):
-    #     return f_res.x[0]
-    #
-    # return x_b_res.x[0]
-
 
 def lamb(x):
     return np.array([-(2*x - 10)])
@@ -113,12 +66,9 @@ def lamb(x):
 
 def test_optimize():
 
-    cons = [{'type': 'ineq', 'fun': lambda x: lamb(x[0])},
-            {'type': 'ineq', 'fun': lambda x: np.array([-(-x[0] - 30)])},
-            {'type': 'ineq', 'fun': lambda x: np.array([-(7*x[0] - 30)])}]
+    cons = [{'type': 'ineq', 'fun': lambda x: np.array([(4 - 3*x[0])])}]
 
-    res = minimize(func, [0.0], args=(-1.0,), constraints=cons, method='SLSQP')
-
-
+    res = minimize(func, 0.0, args=(-1.0,), constraints=cons, method='SLSQP')
+    print(res.x)
 
 # test_optimize()
