3bb0a6214afb3877405cc2166735b28160e2a59b
mherbaghinyan <mbaghinyan@gmail.com>
2017-03-19 18:02:28 +0400

    cooperative report window final

--- project_files/cooperative_report_gui.py
+++ project_files/cooperative_report_gui.py
@@ -7,7 +7,7 @@ from project_files.services.graph_gui import *
 def graph_c_window(root, matrix, t_value):
     graph_c_root = Toplevel(root)
     graph_c_root.title("cooperative game report")
-    graph_c_root.geometry("930x400")
+    graph_c_root.geometry("1000x400")
 
     xf = Frame(graph_c_root, relief=GROOVE, borderwidth=2)
     Label(graph_c_root, text='parametric values of cooperation matrix ').place(relx=0.1, rely=0.10, anchor=NW)
@@ -21,13 +21,17 @@ def graph_c_window(root, matrix, t_value):
             f_vec[f_indx] = matrix[i][j]
             f_indx += 1
 
-    def create_graph_window(i):
-        graph_window(graph_c_root, f_vec[i], 0, 1.1)
+    def create_graph_window(f_index):
+        graph_window(graph_c_root, f_vec[f_index], 0, 1.1)
 
     # enter matrix
-    for i in range(len(f_vec)):
-        Label(xf, text=str(f_vec[i]), relief=GROOVE).grid(row=i + 1, column=1, padx=10, pady=10)
-        Button(xf, text='draw item graph', command= lambda i=i: create_graph_window(i)).grid(row=1 + i, column=2, padx=10, pady=10)
+    f_index = 0
+    for i in range(len(matrix)):
+        for j in range(len(matrix)):
+            Label(xf, text=str(f_vec[f_index]), relief=GROOVE).grid(row=f_index + 1, column=1, padx=10, pady=10)
+            Label(xf, text='i= ' + str(i) + ' j= ' + str(j), relief=GROOVE).grid(row=f_index + 1, column=0, padx=10, pady=10)
+            Button(xf, text='draw item graph', command= lambda f_index=f_index: create_graph_window(f_index)).grid(row=1 + f_index, column=2, padx=10, pady=10)
+            f_index += 1
 
     # pf = Frame(graph_c_root, relief=GROOVE, borderwidth=2)
     # pf.place(relx=0.1, rely=0.4, anchor=NW)
